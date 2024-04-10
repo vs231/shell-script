b38a39a8541f07e1fe9a5d1c86e707365b30bbbb
venkat <pvsandeep21@gmail.com>
2024-04-10 12:23:57 -0500

    loop files

---
+++ loops.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+ID=$(id)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+TIMES=$(date +%F -%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMES.log"
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo " $R Error $N package can't install "
+        exit 1
+    else
+        echo "$N package $G successfully installed"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e " $R Error $N you are not root user"
+    exit 1
+else
+    echo -e "$N you are a $G root user"
+fi
+for package in $@
+do
+    yum list installed $package &>> LOGFILE
+    if [ $? -ne 0 ]
+    then
+        yum install $package -y &>> LOGFILE
+        VALIDATE $? "installation of $package" 
+    else
+        echo " $package is already installed "
+    fi
+done
